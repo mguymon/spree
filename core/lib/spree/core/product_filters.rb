f375587b11e7e9840aad6fc6eff0b20c87ca8bf8
Michael Guymon <michael.guymon@gmail.com>
2013-09-05 14:44:08 -0400

    check for nil

--- core/lib/spree/core/product_filters.rb
+++ core/lib/spree/core/product_filters.rb
@@ -109,7 +109,7 @@ module Spree
 
         def ProductFilters.brand_filter
           brand_property = Spree::Property.find_by(name: 'brand')
-          brands = Spree::ProductProperty.where(property_id: brand_property.id).pluck(:value).uniq.map(&:to_s)
+          brands = Spree::ProductProperty.where(property_id: brand_property.try(:id)).pluck(:value).uniq.map(&:to_s)
           pp = Spree::ProductProperty.arel_table
           conds = Hash[*brands.map { |b| [b, pp[:value].eq(b)] }.flatten]
           {
@@ -148,7 +148,7 @@ module Spree
         def ProductFilters.selective_brand_filter(taxon = nil)
           taxon ||= Spree::Taxonomy.first.root
           brand_property = Spree::Property.find_by(name: 'brand')
-          scope = Spree::ProductProperty.where(property_id: brand_property.id).
+          scope = Spree::ProductProperty.where(property_id: brand_property.try(:id)).
             joins(product: :taxons).
             where("#{Spree::Taxon.table_name}.id" => [taxon] + taxon.descendants).
             scoped
